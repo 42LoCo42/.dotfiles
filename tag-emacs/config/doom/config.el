37500e77e8adfdebec123e7c52a9540708f49acc
42LoCo42 <leonsch@protonmail.com>
2021-04-26 10:19:11 +0200

    Add Doom Emacs config

---
+++ tag-emacs/config/doom/config.el
@@ -0,0 +1,107 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Leon Schumacher"
+      user-mail-address "leonsch@protonmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(setq doom-font "PragmataPro-10")
+
+(defun my/split-and-switch-below ()
+  "Split window below and switch to it."
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
+
+(map! "C-x 2" #'my/split-and-switch-below)
+
+(defun my/split-and-switch-right ()
+  "Split window right and switch to it."
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+
+(map! "C-x 3" #'my/split-and-switch-right)
+
+(setq switch-window-input-style 'minibuffer)
+(setq switch-window-increase 4)
+(setq switch-window-threshold 2)
+(setq switch-window-shortcut-style 'qwerty)
+(setq switch-window-qwerty-shortcuts
+      '("a" "s" "d" "f" "j" "k" "l"))
+(map! "C-x o" #'switch-window)
+
+(map! "M-s" #'avy-goto-char)
+(map! "M-l" #'avy-goto-line)
+
+(map! "<f5>" (lambda ()
+               (interactive)
+               (save-buffer)
+               (setq-local compilation-read-command nil)
+               (call-interactively #'compile)))
+
+(use-package! zig-mode
+  :hook ((zig-mode . lsp-deferred))
+  :custom (zig-format-on-save nil)
+  :config
+  (after! lsp-mode
+    (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
+    (lsp-register-client
+     (make-lsp-client
+      :new-connection (lsp-stdio-connection "/home/leonsch/zls/zls")
+      :major-modes '(zig-mode)
+      :server-id 'zls))))
+
+(setq company-minimum-prefix-length 0)
+(setq company-dabbrev-downcase 0)
+(setq company-idle-delay 0)
