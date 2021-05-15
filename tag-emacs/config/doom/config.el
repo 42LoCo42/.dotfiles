d765101992e39fd382b34289523f51481b34f4c1
42LoCo42 <leonsch@protonmail.com>
2021-05-15 19:32:40 +0200

    Change indent guides

--- tag-emacs/config/doom/config.el
+++ tag-emacs/config/doom/config.el
@@ -119,15 +119,16 @@
   (local-set-key (kbd "TAB") #'tab-to-tab-stop)
   (setq indent-tabs-mode t)
   (setq tab-width custom-tab-width))
+
 (add-hook 'prog-mode-hook 'enable-tabs)
 (add-hook 'lisp-mode-hook 'disable-tabs)
 (add-hook 'emacs-lisp-mode-hook 'disable-tabs)
+(add-hook 'zig-mode-hook 'disable-tabs)
+
 (setq-default electric-indent-inhibit t)
 (setq whitespace-style '(face tabs tab-mark trailing))
-(custom-set-faces
- '(whitespace-tab ((t (:foreground "#636363")))))
 (setq whitespace-display-mappings
-      '((tab-mark 9 [8594 9])))
+      '((tab-mark 9 [124 9])))
 (global-whitespace-mode)
 
 ;; foo
