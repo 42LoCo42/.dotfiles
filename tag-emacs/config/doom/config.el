bd493352ef9d9f7f81f2abb18a75c461042b20b7
Leon Schumacher <leonsch@protonmail.com>
2021-10-22 00:50:32 +0200

    Clean up emacs config

--- tag-emacs/config/doom/config.el
+++ tag-emacs/config/doom/config.el
@@ -1,127 +1,47 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+;;; VARIABLES
 
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Leon Schumacher"
-      user-mail-address "leonsch@protonmail.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
+;; basics
+(setq user-full-name "Leon Schumacher" user-mail-address "leonsch@protonmail.com")
 (setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
 (setq doom-font "Iosevka-10")
-
+(setq doom-theme 'doom-gruvbox)
 (setq fancy-splash-image "~/.config/doom/splash.png")
 
-(defun my/split-and-switch-below ()
-  "Split window below and switch to it."
-  (interactive)
-  (split-window-below)
-  (balance-windows)
-  (other-window 1))
+;; whitespace and tabs
+(setq-default electric-indent-inhibit t)
+(setq whitespace-style '(face tabs tab-mark trailing))
+(setq whitespace-display-mappings
+      '((tab-mark 9 [124 9])))
+(global-whitespace-mode)
+(setq backward-delete-char-untabify-method nil)
 
-(map! "C-x 2" #'my/split-and-switch-below)
+;; paren style
+(setq show-paren-style 'expression)
+(setq show-paren-delay 0)
+(custom-set-faces!
+  '(show-paren-match :bold nil :foreground nil))
 
-(defun my/split-and-switch-right ()
-  "Split window right and switch to it."
-  (interactive)
-  (split-window-right)
-  (balance-windows)
-  (other-window 1))
-
-(map! "C-x 3" #'my/split-and-switch-right)
-
+;; switch-window
 (setq switch-window-input-style 'minibuffer)
 (setq switch-window-increase 4)
 (setq switch-window-threshold 2)
 (setq switch-window-shortcut-style 'qwerty)
 (setq switch-window-qwerty-shortcuts
       '("a" "s" "d" "f" "j" "k" "l"))
-(map! "C-x o" #'switch-window)
 
-(map! "C-#" #'next-window-any-frame)
-(map! "C-M-#" #'previous-window-any-frame)
-
-(map! "M-s" #'avy-goto-char)
-(map! "M-l" #'avy-goto-line)
-
-(map! "<f5>" (lambda ()
-               (interactive)
-               (save-buffer)
-               (setq-local compilation-read-command nil)
-               (call-interactively #'compile)))
-
-(use-package! zig-mode
-  :hook ((zig-mode . lsp-deferred))
-  :custom (zig-format-on-save nil)
-  :config
-  (after! lsp-mode
-    (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
-    (lsp-register-client
-     (make-lsp-client
-      :new-connection (lsp-stdio-connection "zls")
-      :major-modes '(zig-mode)
-      :server-id 'zls))))
-
+;; company
 (setq company-minimum-prefix-length 1)
 (setq company-dabbrev-ignore-case t)
 (setq company-dabbrev-downcase nil)
 (setq company-idle-delay 0)
 
+;; elcord
 (setq elcord-use-major-mode-as-main-icon t)
 (setq elcord-editor-icon "emacs_icon")
-
-(defun my/elcord-buffer-details-format ()
-  (let ((pname (doom-project-name)))
-    (pcase pname
-      ("-" (buffer-name))
-      (_ (format "%s - %s" pname (buffer-name))))))
 (setq elcord-buffer-details-format-function #'my/elcord-buffer-details-format)
-
 (setq elcord-client-id "856822158574878751")
 (setq elcord-mode-icon-alist '((c++-mode . "cpp-mode_icon")
                                (c-mode . "c-mode_icon")
@@ -144,38 +64,51 @@
                                (zig-mode . "zig-mode_icon")))
 (elcord-mode)
 
-(map! "M-+" #'doom/increase-font-size)
-(map! "M-=" #'doom/reset-font-size)
+;; centaur-tabs
+(setq centaur-tabs-gray-out-icons nil)
+(setq centaur-tabs-set-close-button nil)
 
-(defun disable-tabs (&optional width)
-  "Disable tabs, optionally specify indent width"
-  (unless width (setq width 4))
-  (setq indent-tabs-mode nil)
-  (setq tab-width width))
+;;; FUNCTIONS
 
-(defun enable-tabs (&optional width)
-  "Enable tabs, optionally specify tab width"
-  (unless width (setq width 4))
-  (local-set-key (kbd "TAB") #'tab-to-tab-stop)
-  (setq indent-tabs-mode t)
-  (setq tab-width width))
+(defun my/indentall ()
+  "Clean indentation in the whole buffer"
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil))
 
-(add-hook 'prog-mode-hook 'enable-tabs)
-(add-hook 'sh-mode-hook 'enable-tabs)
+(defun my/split-and-switch-below ()
+  "Split window below and switch to it."
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
 
-(add-hook 'haskell-mode-hook (disable-tabs 2))
+(defun my/split-and-switch-right ()
+  "Split window right and switch to it."
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
 
-(add-hook 'lisp-mode-hook 'disable-tabs)
-(add-hook 'emacs-lisp-mode-hook 'disable-tabs)
-(add-hook 'python-mode-hook 'disable-tabs)
-(add-hook 'zig-mode-hook 'disable-tabs)
+(defun my/compile ()
+  "Save & compile buffer"
+  (interactive)
+  (save-buffer)
+  (setq-local compilation-read-command nil)
+  (call-interactively #'compile))
 
-(defun run-special-compiler ()
+(defun my/run-special-compiler ()
   "Runs the special compiler on this file"
   (interactive)
-    (shell-command (concat "compiler " buffer-file-name)))
+  (shell-command (concat "compiler " buffer-file-name)))
 
-(defun open-zathura ()
+(defun my/switch-to-scratch-buffer ()
+  "Switch to the scratch buffer"
+  (interactive)
+  (switch-to-buffer "*scratch*")
+  (lisp-interaction-mode))
+
+(defun my/open-zathura ()
   "Opens zathura on the generated output file"
   (interactive)
   (call-process-shell-command
@@ -184,7 +117,7 @@
     (file-name-sans-extension buffer-file-name)
     ".ps") nil 0))
 
-(defun ps2pdf ()
+(defun my/ps2pdf ()
   "Run the ps2pdf shell command"
   (interactive)
   (call-process-shell-command
@@ -193,48 +126,99 @@
     (file-name-sans-extension buffer-file-name)
     ".ps") nil 0))
 
-(add-hook
- 'nroff-mode-hook
- (lambda ()
-   (add-hook 'after-save-hook 'run-special-compiler)
-   (map! "C-c C-o" 'open-zathura)
-   (map! "C-c C-p" 'ps2pdf)))
+(defun my/disable-tabs (&optional width)
+  "Disable tabs, optionally specify indent width"
+  (unless width (setq width 4))
+  (setq indent-tabs-mode nil)
+  (setq tab-width width))
 
-(setq-default electric-indent-inhibit t)
-(setq whitespace-style '(face tabs tab-mark trailing))
-(setq whitespace-display-mappings
-      '((tab-mark 9 [124 9])))
-(global-whitespace-mode)
+(defun my/enable-tabs (&optional width)
+  "Enable tabs, optionally specify tab width"
+  (unless width (setq width 4))
+  (local-set-key (kbd "TAB") #'tab-to-tab-stop)
+  (setq indent-tabs-mode t)
+  (setq tab-width width))
 
-(defun switch-to-scratch-buffer ()
-  "Switch to the scratch buffer"
-  (interactive)
-  (switch-to-buffer "*scratch*"))
-(map! "C-c x" #'switch-to-scratch-buffer)
+(defun my/elcord-buffer-details-format ()
+  (let ((pname (doom-project-name)))
+    (pcase pname
+      ("-" (buffer-name))
+      (_ (format "%s - %s" pname (buffer-name))))))
 
-(setq backward-delete-char-untabify-method nil)
+;;; HOOKS
 
-(map! "<C-tab>" (lambda ()
-                  (interactive)
-                  (delete-trailing-whitespace)
-                  (indent-region (point-min) (point-max) nil)))
+;; enable tabs everywhere
+(add-hook 'prog-mode-hook #'my/enable-tabs)
+(add-hook 'sh-mode-hook   #'my/enable-tabs)
 
-(map! "C-v" #'yank)
-(map! "M-v" #'counsel-yank-pop)
-(map! "C-z" #'undo-fu-only-undo)
-(map! "C-y" #'undo-fu-only-redo)
-(map! "M-n" #'scroll-up-command)
-(map! "M-p" #'scroll-down-command)
-(map! "C-w" #'kill-ring-save)
-(map! "M-w" #'kill-region)
-(map! "C-." #'mc/mark-next-like-this)
-(map! "C-," #'mc/mark-previous-like-this)
+;; disable tabs in some modes
+(add-hook 'haskell-mode-hook    (my/disable-tabs 2))
+(add-hook 'lisp-mode-hook       #'my/disable-tabs)
+(add-hook 'emacs-lisp-mode-hook #'my/disable-tabs)
+(add-hook 'python-mode-hook     #'my/disable-tabs)
+(add-hook 'zig-mode-hook        #'my/disable-tabs)
 
-(setq show-paren-style 'expression)
-(setq show-paren-delay 0)
-(custom-set-faces!
-  '(show-paren-match :bold nil :foreground nil))
+;; nroff uses some functions
+(add-hook 'nroff-mode-hook (lambda ()
+                             (add-hook 'after-save-hook #'my/run-special-compiler)
+                             (map! "C-c C-o" #'my/open-zathura)
+                             (map! "C-c C-p" #'my/ps2pdf)))
 
-(map! "M-+" 'text-scale-increase)
-(map! "M--" 'text-scale-decrease)
-(map! "M-=" (lambda () (interactive) (text-scale-set 0)))
+;; zig config
+(use-package! zig-mode
+  :hook ((zig-mode . lsp-deferred))
+  :custom (zig-format-on-save nil)
+  :config
+  (after! lsp-mode
+    (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
+    (lsp-register-client
+     (make-lsp-client
+      :new-connection (lsp-stdio-connection "zls")
+      :major-modes '(zig-mode)
+      :server-id 'zls))))
+
+;;; BINDINGS
+
+(defvar my-keys-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    ;; Editing
+    (map! "<C-tab>" #'my/indentall)
+    (map! "C-v"     #'yank)
+    (map! "M-v"     #'counsel-yank-pop)
+    (map! "C-w"     #'kill-ring-save)
+    (map! "M-w"     #'kill-region)
+    (map! "C-y"     #'undo-fu-only-redo)
+    (map! "C-z"     #'undo-fu-only-undo)
+
+    ;; Movement
+    (map! "C-," #'mc/mark-previous-like-this)
+    (map! "C-." #'mc/mark-next-like-this)
+    (map! "M-l" #'avy-goto-line)
+    (map! "M-s" #'avy-goto-char)
+    (map! "M-n" #'scroll-up-command)
+    (map! "M-p" #'scroll-down-command)
+
+    ;; Window controls
+    (map! "C-#"       #'next-window-any-frame)
+    (map! "C-M-#"     #'previous-window-any-frame)
+    (map! "C-<next>"  #'centaur-tabs-forward)
+    (map! "C-<prior>" #'centaur-tabs-backward)
+    (map! "C-x 2"     #'my/split-and-switch-below)
+    (map! "C-x 3"     #'my/split-and-switch-right)
+    (map! "C-x o"     #'switch-window)
+
+    ;; Tools
+    (map! "<F5>"  #'my/compile)
+    (map! "C-c x" #'my/switch-to-scratch-buffer)
+    (map! "M-+"   #'text-scale-increase)
+    (map! "M--"   #'text-scale-decrease)
+    (map! "M-="   (lambda (interactive) (text-scale-set 0)))
+
+    map)
+  "my-keys-minor-mode keymap.")
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mode for my keybindings"
+  :init-value t)
+
+(my-keys-minor-mode)
