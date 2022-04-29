75b76c15ec4e224c4038c354eeeec432d14a8444
Leon Schumacher <leonsch@protonmail.com>
2022-04-29 16:41:56 +0200

    Install scripts are no longer hooks

--- install.sh
+++ install.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
-set -e
-echo "Installing primary dependencies..."
-sudo pacman -S --needed --noconfirm base-devel git
-
-if ! command -v yay >/dev/null; then
-	yay="$HOME/yay-bin"
-	git clone https://aur.archlinux.org/yay-bin "$yay"
-	pushd "$yay" || exit 1
-	makepkg -cfi --noconfirm
-	popd
-	rm -rf "$yay"
-fi
-
-if ! command -v rcup >/dev/null; then
-	yay -S --noconfirm rcm
-fi
-
-exec rcup
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" || exit 1
+for script in scripts/*; do
+	. "$script"
+done
