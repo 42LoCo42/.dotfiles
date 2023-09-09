1b0bedc5a35ca74c0e7e7a8d9a09dcde7070d7ec
Eleonora <leonsch@protonmail.com>
2023-09-09 16:59:00 +0200

    Add password hash fallback logic

--- modules/base/user.nix
+++ modules/base/user.nix
@@ -4,7 +4,20 @@
     name = "leonsch";
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    initialPassword = " ";
+    initialHashedPassword =
+      # use hash from secret file only when it's in a known (decrypted) state
+      # otherwise use " " (single space) as password
+      # this is ugly
+      let
+        file = ./hash.secret;
+        realHash = builtins.hashFile "sha256" file;
+        wantHash = "e7b44e5e59594b3ee78549525167ce83d9d368df769f49a5bd1d2525aebb9d88";
+        real = builtins.replaceStrings [ "\n" ] [ "" ] (builtins.readFile file);
+        space = builtins.trace
+          "warning: using \" \" (single space) as password - please decrypt hash.secret!"
+          "$y$j9T$rs6onrT38/cnFRJEUFnIV1$LWZcBd5R5y7iTKsPgseCaws2InrhhyJQGdULSPsrmJ6";
+      in
+      if realHash == wantHash then real else space;
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVieLCkWGImVI9c7D0Z0qRxBAKf0eaQWUfMn0uyM/Ql" ];
   };
 
