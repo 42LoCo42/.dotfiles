fe27ae5029fe551aab211ff5042c6f1e1582a27f
Eleonora <leonsch@protonmail.com>
2023-09-26 13:07:13 +0200

    shell: add list of currently `use`d packages

--- modules/home/terminal.nix
+++ modules/home/terminal.nix
@@ -66,6 +66,10 @@
       starship = {
         enable = true;
         settings = {
+          custom.usepkgs = {
+            command = "_usepkgs";
+            when = ''[ -n "$IN_USE_SHELL" ]'';
+          };
           character = {
             success_symbol = "[λ](bold green)";
             error_symbol = "[λ](bold red)";
