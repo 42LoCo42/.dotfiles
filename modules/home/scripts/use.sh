8a2cf237c14ad2c98c139c59b3112232206046b9
Leon Schumacher <leonsch@protonmail.com>
2023-10-24 16:08:32 +0200

    use.sh: link to current nixpkgs input

--- modules/home/scripts/use.sh
+++ modules/home/scripts/use.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-# get nixpkgs flake path currently in use by the system
-# this removes the need to download nixpkgs everytime nix shell is executed
-# shellcheck disable=SC2016
-nixpkgs="$(nix eval --raw --impure --expr '"${(builtins.getFlake "'"$HOME/config"'").inputs.nixpkgs}"')"
-
 pkgs=()
 for pkg in "$@"; do
 	# prepend our nixpkgs path if the argument is just a package name
-	grep -q '#' <<< "$pkg" || pkg="$nixpkgs#$pkg"
+	grep -q '#' <<< "$pkg" || pkg="@nixpkgs@#$pkg"
 	echo "Using $pkg"
 	pkgs+=("$pkg")
 done
