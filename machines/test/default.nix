b558fc4c943b64c8fb80eb49f3e1567c13ea9ab9
Leon Schumacher <leonsch@protonmail.com>
2023-10-24 15:02:05 +0200

    [WIP] Switch to nixinate

--- machines/test/default.nix
+++ machines/test/default.nix
@@ -2,8 +2,10 @@
   system = "x86_64-linux";
   specialArgs = args;
   modules = [
+    ./disk.nix
     ./hardware.nix
     self.inputs.home-manager.nixosModules.home-manager
+    self.inputs.disko.nixosModules.disko
 
     "${self}/modules/base/all.nix"
     "${self}/modules/boot/zfs.nix"
@@ -11,12 +13,24 @@
     "${self}/modules/networking/default.nix"
 
     ({ lib, ... }: {
+      _module.args.nixinate = {
+        host = "192.168.122.66";
+        sshUser = "leonsch";
+        buildOn = "remote";
+        hermetic = false;
+      };
+
       networking = {
         hostName = "test";
         hostId = "a5d4deab";
 
         networkmanager.enable = lib.mkForce false;
       };
+
+      # virtualisation.docker = {
+      #   enable = true;
+      #   storageDriver = "zfs";
+      # };
     })
   ];
 }
--- machines/test/hardware.nix
+++ machines/test/hardware.nix
@@ -6,17 +6,6 @@
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
   boot.kernelModules = [ "kvm-intel" ];
 
-  fileSystems."/" = {
-    device = "rpool/nixos";
-    fsType = "zfs";
-    options = [ "zfsutil" ];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/9BA4-9C24";
-    fsType = "vfat";
-  };
-
   networking.interfaces.enp1s0.useDHCP = true;
   nixpkgs.hostPlatform = "x86_64-linux";
 }
