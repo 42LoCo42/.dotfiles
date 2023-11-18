86f32d6afbe2edef9f10f5a75e746c45f7d6b58d
Eleonora <leonsch@protonmail.com>
2023-11-18 08:10:29 +0100

    agenix: add key for akyuro, add program to system environment

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,12 @@
 let
   # machines
+  akyuro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjSkmdFrVrraCSwZZq7spsSMv43L8y68FflQhu2VqmP";
   test = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcCguBV6loAZZyJ2PCqQUxpZJy4H4YLdoZcoXkq65qY";
 
   # users
   leonsch = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVieLCkWGImVI9c7D0Z0qRxBAKf0eaQWUfMn0uyM/Ql";
 
-  all = [ test leonsch ];
+  all = [ akyuro test leonsch ];
 in
 {
   "password-hash.age".publicKeys = all;
