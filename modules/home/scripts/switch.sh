6e59dd158855b88af0dbcf91113ca3efb08b7097
Eleonora <leonsch@protonmail.com>
2023-11-18 08:38:46 +0100

    switch.sh: link new configuration to /nix/var/nix/profiles via nix-env

--- modules/home/scripts/switch.sh
+++ modules/home/scripts/switch.sh
@@ -5,7 +5,6 @@ new="$("@nom@" \
 	build --no-link --print-out-paths \
 	"path:$HOME/config#nixosConfigurations.$(hostname).config.system.build.toplevel")"
 "@nvd@" diff "/run/current-system" "$new"
-# read -rp "Switch to configuration? [y/N]"
-# [ "${REPLY,,}" == "y" ] && \
-read -rp "Press ENTER to confirm or ctrl-c to cancel"
+echo "Activating configuration..."
+sudo nix-env --set --profile /nix/var/nix/profiles/system "$new"
 sudo "$new/bin/switch-to-configuration" switch
