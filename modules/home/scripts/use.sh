52432e54477af77998582016a59b362adb5dc78c
Eleonora <leonsch@protonmail.com>
2023-11-18 08:15:11 +0100

    use.sh: accept flags, URLs and package references

--- modules/home/scripts/use.sh
+++ modules/home/scripts/use.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-pkgs=()
-for pkg in "$@"; do
-	# prepend our nixpkgs path if the argument is just a package name
-	grep -q '#' <<< "$pkg" || pkg="@nixpkgs@#$pkg"
-	echo "Using $pkg"
-	pkgs+=("$pkg")
+args=()
+for arg in "$@"; do
+	# prepend nixpkgs if the argument is only a package name
+	grep -q '^-\|[#:]' <<< "$arg" || {
+		arg="@nixpkgs@#$arg"
+		echo "Using $arg"
+	}
+	args+=("$arg")
 done
 
 export IN_USE_SHELL=1
-exec nix shell "${pkgs[@]}"
+exec nix shell -L "${args[@]}"
