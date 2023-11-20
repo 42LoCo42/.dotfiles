7d49a6d815d577527f3c9e4510a5e9d7d630a5a0
Leon Schumacher <leonsch@protonmail.com>
2023-11-20 14:23:28 +0100

    secrets: discover all age files

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,9 +6,23 @@ let
   # users
   leonsch = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVieLCkWGImVI9c7D0Z0qRxBAKf0eaQWUfMn0uyM/Ql";
 
-  all = [ akyuro test leonsch ];
+  publicKeys = [ akyuro test leonsch ];
+
+  # helper functions taken from nixpkgs.lib
+
+  pipe = val: functions:
+    let reverseApply = x: f: f x;
+    in builtins.foldl' reverseApply val functions;
+
+  filterAttrs = pred: set:
+    builtins.listToAttrs (builtins.concatMap
+      (name:
+        let value = set.${name}; in
+        if pred name value then [{ inherit name value; }] else [ ])
+      (builtins.attrNames set));
 in
-{
-  "password-hash.age".publicKeys = all;
-  "id_ed25519.age".publicKeys = all;
-}
+pipe ./. [
+  builtins.readDir
+  (filterAttrs (name: _: builtins.match ".*\.age" name != null))
+  (builtins.mapAttrs (name: _: { inherit publicKeys; }))
+]
