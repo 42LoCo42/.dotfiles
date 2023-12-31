807c67fbc448eef657e750806b70d00d2b9e3136
Eleonora <leonsch@protonmail.com>
2023-12-31 14:19:25 +0100

    Remove nyxpkgs (foot-transparent is in obscura now)

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -77,7 +77,7 @@
     programs = {
       foot = {
         enable = true;
-        package = self.inputs.nyxpkgs.packages.${pkgs.system}.foot-transparent;
+        package = self.inputs.obscura.packages.${pkgs.system}.foot-transparent;
         settings = {
           main = {
             font = "monospace:size=10.5";
