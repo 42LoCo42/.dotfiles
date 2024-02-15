b97db3cf825bd9797b3ba1d7d70e003672b062f6
Eleonora <leonsch@protonmail.com>
2024-02-15 09:01:50 +0100

    update

--- flake.nix
+++ flake.nix
@@ -1,15 +1,19 @@
 {
   inputs = {
     aquaris.url = "github:42loco42/aquaris";
+    aquaris.inputs.home-manager.follows = "home-manager";
     aquaris.inputs.nixpkgs.follows = "nixpkgs";
 
-    # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-index-database.url = "github:Mic92/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
 
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     obscura.url = "github:42loco42/obscura";
   };
 
-  outputs = { self, aquaris, nixpkgs, ... }: aquaris.lib.aquarisSystems self;
+  outputs = { self, aquaris, ... }: aquaris.lib.aquarisSystems self;
 }
