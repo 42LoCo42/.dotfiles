b5674e3902c9509cc60d53b6f9f27fa330c49523
Leon Schumacher <leonsch@protonmail.com>
2024-02-21 12:39:25 +0100

    Update Aquaris

--- secrets/secrets.nix
+++
@@ -1,6 +0,0 @@
-let
-  f = builtins.fromJSON (builtins.readFile ../flake.lock);
-  n = f.nodes.${f.nodes.${f.root}.inputs.aquaris}.locked;
-  aquaris = builtins.getFlake "${n.type}:${n.owner}/${n.repo}/${n.rev}";
-in
-aquaris.lib.secretsHelper ./..
