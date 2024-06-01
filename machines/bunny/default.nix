ee81a80767ff7c9bc02f27c64cea91fee87861f7
Leon Schumacher <leonsch@protonmail.com>
2024-06-01 20:07:29 +0200

    bunny: pure Nix OCI containers (hell yeah)

--- machines/bunny/default.nix
+++ machines/bunny/default.nix
@@ -1,4 +1,41 @@
-{ self, pkgs, config, lib, my-utils, ... }: {
+{ self, pkgs, config, lib, my-utils, ... }:
+let
+  inherit (lib) getExe getExe' pipe;
+
+  domain = "eleonora.gay";
+  subsDomain = file: my-utils.subsF {
+    inherit file;
+    func = pkgs.writeText;
+    subs = { inherit domain; };
+  };
+
+  iosevka = pipe pkgs.nerdfonts [
+    (x: x.override { fonts = [ "Iosevka" ]; })
+    (x: "${x}/share/fonts/truetype/NerdFonts/IosevkaNerdFont-Regular.ttf")
+  ];
+
+  homepage = pkgs.stdenvNoCC.mkDerivation {
+    name = "homepage";
+    src = let d = ../../homepage; in lib.fileset.toSource {
+      root = d;
+      fileset = d;
+    };
+
+    nativeBuildInputs = with pkgs; [
+      glibcLocales
+      tree
+      self.inputs.obscura.packages.${pkgs.system}.pug
+    ];
+
+    buildPhase = ''
+      cp -r static $out
+      cp "${iosevka}" $out/iosevka.ttf
+      bash processStuff.sh
+      pug3 -o $out .
+    '';
+  };
+in
+{
   aquaris = {
     filesystem = { filesystem, zpool, ... }: {
       disks."/dev/disk/by-id/scsi-36024c6ac39264da98ce1a64b9fab7a20".partitions = [
@@ -21,13 +58,14 @@
     };
 
     secrets = {
-      "machine/synapse/secrets".user = "nobody";
-      "machine/synapse/signing-key".user = "nobody";
+      "machine/synapse/secrets".user = "synapse";
+      "machine/synapse/signing-key".user = "synapse";
     };
 
     persist = {
       system = [
         "/var/lib/containers"
+        "/var/lib/nixos"
       ];
       users.admin = [
         "hidden"
@@ -56,7 +94,6 @@
     self.inputs.obscura.packages.${pkgs.system}.pug
   ];
 
-  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   networking.firewall.allowedUDPPorts = [ 443 ];
   networking.networkmanager.enable = lib.mkForce false;
@@ -71,214 +108,122 @@
 
   systemd.services.podman-volume-setup.serviceConfig.Restart = lib.mkForce "on-failure";
   virtualisation.podman.defaultNetwork.settings.dns_enabled = true;
-  virtualisation.oci-containers.containers =
-    let
-      inherit (lib) getExe getExe' pipe;
 
-      domain = "eleonora.gay";
-      subsDomain = file: my-utils.subsF {
-        inherit file;
-        func = pkgs.writeText;
-        subs = { inherit domain; };
+  virtualisation.pnoc = {
+    caddy = {
+      cmd = [ (getExe pkgs.caddy) "run" "-a" "caddyfile" "-c" "${./Caddyfile}" ];
+      environment = {
+        DOMAIN = domain;
+        XDG_DATA_HOME = "/";
       };
-
-      user = pipe [
-        config.users.users.nobody.uid
-        config.users.groups.nogroup.gid
-      ] [
-        (map (x: builtins.toString x))
-        (builtins.concatStringsSep ":")
+      ports = [
+        "80:8000"
+        "443:4430"
+        "443:4430/udp"
       ];
-
-      mkVolumeSetup = volumes:
-        let
-          program = pkgs.writeShellApplication {
-            name = "volume-setup";
-            runtimeInputs = with pkgs; [ coreutils ];
-            text = pipe volumes [
-              (map (v: "chown ${user} /vol/${v}"))
-              (s: [ "set -x" ] ++ s)
-              (builtins.concatStringsSep "\n")
-            ];
-          };
-
-          imageFile = pkgs.dockerTools.buildImage {
-            name = "volume-setup";
-            tag = "latest";
-            config.Cmd = [ (getExe program) ];
-          };
-        in
-        {
-          inherit imageFile;
-          image = "volume-setup:latest";
-          volumes = map (v: "${v}:/vol/${v}") volumes;
-        };
-
-      homepage-font = pipe pkgs.nerdfonts [
-        (f: f.override { fonts = [ "Iosevka" ]; })
-        (f: "${f}/share/fonts/truetype/NerdFonts/IosevkaNerdFont-Regular.ttf")
+      ssl = true;
+      volumes = [
+        "caddy:/caddy"
+        "${homepage}:/srv/homepage"
+        "/persist/home/admin/hidden:/srv/homepage/foo"
+        "${pkgs.element-web}:/srv/element"
+        "${subsDomain ./element.json}:/srv/element/config.json"
       ];
-
-      homepage = pkgs.stdenvNoCC.mkDerivation {
-        name = "homepage";
-        src = let d = ../../homepage; in lib.fileset.toSource {
-          root = d;
-          fileset = d;
-        };
-
-        nativeBuildInputs = with pkgs; [
-          glibcLocales
-          tree
-          self.inputs.obscura.packages.${pkgs.system}.pug
-        ];
-
-        buildPhase = ''
-          cp -r static $out
-          cp "${homepage-font}" $out/iosevka.ttf
-          bash processStuff.sh
-          pug3 -o $out .
-        '';
-      };
-    in
-    {
-      volume-setup = mkVolumeSetup [
-        "attic_data"
-        "caddy_data"
-        "pigallery2_data"
-        "pinlist_data"
-        "redis_data"
-        "synapse_data"
-        "vaultwarden_data"
-      ];
-
-      caddy = {
-        inherit user;
-        image = "caddy@sha256:2ec95d40ce2d1f18f92eee012a5dd18b352075d92eb6c4f4e0ea18c46ad4b069";
-        ports = [
-          "80:80"
-          "443:443"
-          "443:443/udp"
-        ];
-        volumes = [
-          "caddy_data:/data/caddy"
-          "${./Caddyfile}:/etc/caddy/Caddyfile"
-          "${homepage}:/srv/homepage"
-          "${config.aquaris.persist.root}/home/admin/hidden:/srv/homepage/foo"
-          "${pkgs.element-web}:/srv/element"
-          "${subsDomain ./element.json}:/srv/element/config.json"
-        ];
-        environment.DOMAIN = domain;
-      };
-
-      attic = {
-        inherit user;
-        image = "attic:latest";
-        imageFile = pkgs.dockerTools.buildImage {
-          name = "attic";
-          tag = "latest";
-          config.Cmd = [ (getExe pkgs.attic-server) "-f" "/attic.toml" ];
-          extraCommands = ''
-            ln -s "${getExe' pkgs.attic-server "atticadm"}"
-          '';
-        };
-        volumes = [
-          "attic_data:/data"
-          "${subsDomain ./attic.toml}:/attic.toml"
-        ];
-        environmentFiles = [ config.aquaris.secrets."machine/attic" ];
-      };
-
-      pinlist = {
-        inherit user;
-        image = "pinlist:latest";
-        imageFile = self.inputs.pinlist.packages.${pkgs.system}.image;
-        volumes = [ "pinlist_data:/db" ];
-        environmentFiles = [ config.aquaris.secrets."machine/pinlist" ];
-      };
-
-      redis = {
-        inherit user;
-        image = "redis@sha256:197c38d14b42b59a49ed7d76adf85b93f8c18c1631031841ad26c81c5e11f96e";
-        volumes = [ "redis_data:/data" ];
-      };
-
-      searxng =
-        let
-          su-exec = pkgs.writeScript "su-exec" ''
-            #!/usr/bin/env sh
-            shift # consume user:group
-            exec "$@"
-          '';
-        in
-        {
-          inherit user;
-          image = "searxng/searxng@sha256:719d46534dbef6444d972172b1a4b1daa8e913ed35b2bb0ff30499bffe4b4240";
-          extraOptions = [ "--stop-signal=SIGINT" ];
-          volumes = [
-            "${./searxng.yaml}:/etc/searxng/settings.yml"
-            "${su-exec}:/sbin/su-exec"
-          ];
-          environmentFiles = [ config.aquaris.secrets."machine/searxng" ];
-          environment.SEARXNG_BASE_URL = "https://searx.${domain}";
-        };
-
-      synapse = {
-        inherit user;
-        image = "matrixdotorg/synapse@sha256:8816e729ef77fc3f79f39e6c38ffc73388059c0eac3c34a13b0d11f6d61ab64a";
-        volumes = [
-          "synapse_data:/data"
-          "${subsDomain ./synapse.yaml}:/config/homeserver.yaml"
-          "${config.aquaris.secrets."machine/synapse/secrets"}:/config/secrets.yaml"
-          "${config.aquaris.secrets."machine/synapse/signing-key"}:/config/signing.key"
-        ];
-        cmd = [ "run" "-c" "/config" ];
-      };
-
-      synapse-db = {
-        image = "postgres@sha256:354a818d8a1e94707704902edb8c4e98b0eb64de3ee0354c4d94b4e2905c63ee";
-        volumes = [ "synapse-db_data:/db" ];
-        environment = {
-          POSTGRES_INITDB_ARGS = "--encoding=UTF8 --locale=C";
-          PGDATA = "/db";
-        };
-        environmentFiles = [ config.aquaris.secrets."machine/synapse/db-password" ];
-      };
-
-      pigallery2 = {
-        inherit user;
-        image = "bpatrik/pigallery2@sha256:e3a260d8eff86ea9e5b16f905ef7ce342e20654ee7e782f49a23acc204bb5300";
-        extraOptions = [ "--health-cmd=none" ];
-        volumes = [
-          "${subsDomain ./pigallery2.json}:/app/data/config/config.json"
-          "${config.aquaris.persist.root}/home/admin/img:/media"
-          "pigallery2_data:/data"
-        ];
-        environment.NODE_ENV = "production";
-      };
-
-      vaultwarden = {
-        inherit user;
-        image = "vaultwarden/server@sha256:edb8e2bab9cbca22e555638294db9b3657ffbb6e5d149a29d7ccdb243e3c71e0";
-        volumes = [ "vaultwarden_data:/data" ];
-        environment = {
-          DOMAIN = "https://vw.${domain}";
-          ROCKET_PORT = "8080";
-          SIGNUPS_ALLOWED = "false";
-
-          SMTP_HOST = "smtp.gmail.com";
-          SMTP_FROM = "vault@${domain}";
-        };
-        environmentFiles = [ config.aquaris.secrets."machine/vaultwarden" ];
-      };
-
-      avh = {
-        inherit user;
-        image = "avh:latest";
-        imageFile = self.inputs.avh.packages.${pkgs.system}.foo;
-        volumes = [
-          "/persist/home/admin/avh/users.db:/users.db"
-          "/persist/home/admin/avh/videos:/videos"
-        ];
-      };
     };
+
+    ##### exposed services #####
+
+    attic = {
+      cmd = [ (getExe pkgs.attic-server) "-f" "${subsDomain ./attic.toml}" ];
+      environmentFiles = [ config.aquaris.secrets."machine/attic" ];
+      volumes = [ "attic:/data" ];
+    };
+
+    avh = {
+      cmd = [ (getExe self.inputs.avh.packages.${pkgs.system}.default) ];
+      volumes = [
+        "/persist/home/admin/avh/users.db:/users.db"
+        "/persist/home/admin/avh/videos:/videos"
+      ];
+    };
+
+    pinlist = {
+      cmd = [ (getExe self.inputs.pinlist.packages.${pkgs.system}.default) ];
+      environmentFiles = [ config.aquaris.secrets."machine/pinlist" ];
+      volumes = [ "pinlist:/db" ];
+    };
+
+    searxng = {
+      cmd = [ (getExe' pkgs.searxng "searxng-run") ];
+      environment = {
+        SEARXNG_BIND_ADDRESS = "0.0.0.0";
+        SEARXNG_URL = "https://searx.${domain}";
+      };
+      environmentFiles = [ config.aquaris.secrets."machine/searxng" ];
+      ssl = true;
+      volumes = [ "${./searxng.yaml}:/etc/searxng/settings.yml" ];
+    };
+
+    synapse = {
+      cmd = [ (getExe' pkgs.matrix-synapse "synapse_homeserver") "-c" "/config" ];
+      ssl = true;
+      volumes = [
+        "synapse:/data"
+        "${subsDomain ./synapse.yaml}:/config/homeserver.yaml"
+        "${config.aquaris.secrets."machine/synapse/secrets"}:/config/secrets.yaml"
+        "${config.aquaris.secrets."machine/synapse/signing-key"}:/config/signing.key"
+      ];
+    };
+
+    vaultwarden = {
+      cmd = [ (getExe pkgs.vaultwarden) ];
+      environment = {
+        ROCKET_ADDRESS = "0.0.0.0";
+        ROCKET_PORT = "8080";
+
+        DOMAIN = "https://vw.${domain}";
+        SIGNUPS_ALLOWED = "false";
+
+        SMTP_FROM = "vault@${domain}";
+        SMTP_HOST = "smtp.gmail.com";
+
+        WEB_VAULT_FOLDER = "${pkgs.vaultwarden.webvault}/share/vaultwarden/vault";
+      };
+      environmentFiles = [ config.aquaris.secrets."machine/vaultwarden" ];
+      ssl = true;
+      volumes = [ "vaultwarden:/data" ];
+    };
+
+    ##### support services #####
+
+    postgres = {
+      cmd = [ (getExe' pkgs.postgresql_16 "postgres") "-D" "/data" ];
+      volumes = [
+        "postgres:/data"
+        "postgres:/run/postgresql"
+        "${./postgres/postgresql.conf}:/data/postgresql.conf"
+        "${./postgres/pg_hba.conf}:/data/pg_hba.conf"
+      ];
+    };
+
+    redis = {
+      cmd = [ (getExe' pkgs.redis "redis-server") "--protected-mode" "no" ];
+      volumes = [ "redis:/data" ];
+      workdir = "/data";
+    };
+  };
+
+  virtualisation.oci-containers.containers = {
+    pigallery2 = {
+      user = "65534:65534";
+      image = "bpatrik/pigallery2@sha256:e3a260d8eff86ea9e5b16f905ef7ce342e20654ee7e782f49a23acc204bb5300";
+      extraOptions = [ "--health-cmd=none" ];
+      volumes = [
+        "${subsDomain ./pigallery2.json}:/app/data/config/config.json"
+        "/persist/home/admin/img:/media"
+        "pigallery2_data:/data"
+      ];
+      environment.NODE_ENV = "production";
+    };
+  };
 }
