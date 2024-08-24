1007283583fe39859164017de8a70bef60b32148
Leon Schumacher <leonsch@protonmail.com>
2024-08-24 19:56:31 +0200

    rice/git: sign commits with SSH

---
+++ rice/git.nix
@@ -0,0 +1,28 @@
+{ pkgs, lib, aquaris, ... }: {
+  home-manager.users.leonsch = hm:
+    let
+      inherit (lib) mkForce;
+
+      allowedSigners = pkgs.writeText "allowedSigners" ''
+        ${hm.config.programs.git.userEmail} namespaces="git" ${aquaris.cfg.mainSSHKey}
+      '';
+    in
+    {
+      programs = {
+        git = {
+          extraConfig = {
+            gpg = {
+              format = "ssh";
+              ssh.allowedSignersFile = "${allowedSigners}";
+            };
+
+            merge.tool = "vimdiff";
+          };
+
+          signing.key = mkForce "~/.ssh/id_ed25519";
+        };
+
+        gpg.enable = mkForce false;
+      };
+    };
+}
