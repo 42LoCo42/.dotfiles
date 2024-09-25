90fbf4bfa0e9c028ed972d0fa2442493d274bdaf
Leon Schumacher <leonsch@protonmail.com>
2024-09-25 23:08:18 +0200

    rice/overlay: add more glfw3-minecraft patches

--- rice/overlay.nix
+++ rice/overlay.nix
@@ -15,14 +15,24 @@ in
 
       foot = self.inputs.obscura.packages.${pkgs.system}.foot-transparent;
 
-      glfw3-minecraft = pkgs.glfw3-minecraft.overrideAttrs (old: {
-        patches = old.patches ++ [
-          (pkgs.fetchpatch {
-            url = "https://raw.githubusercontent.com/BoyOrigin/glfw-wayland/f62b4ae8f93149fd754cadecd51d8b1a07d20522/patches/0005-Avoid-error-on-startup.patch";
-            hash = "sha256-oF+mTNOXPq/yr+y58tTeRkLJE67QzJJSleKFZ85+Uys=";
-          })
-        ];
-      });
+      glfw3-minecraft =
+        let
+          fixes = pkgs.fetchFromGitHub {
+            owner = "BoyOrigin";
+            repo = "glfw-wayland";
+            rev = "2024-03-07";
+            hash = "sha256-kvWP34rOD4HSTvnKb33nvVquTGZoqP8/l+8XQ0h3b7Y=";
+          };
+        in
+        pkgs.glfw3-minecraft.overrideAttrs (old: {
+          patches = old.patches ++ [
+            "${fixes}/patches/0001-Key-Modifiers-Fix.patch"
+            "${fixes}/patches/0002-Fix-duplicate-pointer-scroll-events.patch"
+            # "${fixes}/patches/0003-Implement-glfwSetCursorPosWayland.patch"
+            "${fixes}/patches/0004-Fix-Window-size-on-unset-fullscreen.patch"
+            "${fixes}/patches/0005-Avoid-error-on-startup.patch"
+          ];
+        });
 
       mypaint = pkgs.mypaint.overrideAttrs { installCheckPhase = ""; };
 
