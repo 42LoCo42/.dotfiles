7a851eb3168f95177744de0a4f73e19f0431c11a
Leon Schumacher <leonsch@protonmail.com>
2024-10-14 15:50:59 +0200

    rice/eza: add more icons

--- rice/tools/eza.nix
+++ rice/tools/eza.nix
@@ -18,6 +18,7 @@
 
     xdg.configFile."eza/theme.yml".text = builtins.toJSON {
       filenames = {
+        # dot stuff in home
         ".cache".icon.glyph = "󰃨";
         ".ghci".icon.glyph = "";
         ".icons".icon.glyph = "";
@@ -28,21 +29,36 @@
         ".pki".icon.glyph = "󰌾";
         ".zsh".icon.glyph = "";
 
+        # main dirs in home
         "dev".icon.glyph = "";
         "doc".icon.glyph = "󰈙";
         "img".icon.glyph = "";
-        "keys".icon.glyph = "󰢬";
         "music".icon.glyph = "";
-        "secrets".icon.glyph = "󰌾";
         "work".icon.glyph = "";
+
+        # nixos config
+        "homepage".icon.glyph = "󰖟";
+        "images".icon.glyph = "";
+        "keys".icon.glyph = "󰢬";
+        "machines".icon.glyph = "";
+        "rice".icon.glyph = "󰟪";
+        "secrets".icon.glyph = "󰦝";
+
+        # misc
+        ".jj".icon.glyph = "󰘬";
+        "Caddyfile".icon.glyph = "󰒒";
+        "result".icon.glyph = "";
       };
 
       extensions = {
+        "excalidraw".icon.glyph = "";
         "gpx".icon.glyph = "󰖃";
         "json".icon.glyph = "";
+        "key".icon.glyph = "󰌆";
         "ora".icon.glyph = "";
+        "prettierrc".icon.glyph = "";
+        "pug".icon.glyph = "";
         "yaml".icon.glyph = "";
-        "excalidraw".icon.glyph = "";
       };
     };
   }];
