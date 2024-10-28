200780eb63a6d3446a8bcb52e16252472fcc983a
Leon Schumacher <leonsch@protonmail.com>
2024-10-28 10:13:56 +0100

    rice/common/nix: add aliases; use nom v2.1.4

--- rice/common/nix.nix
+++ rice/common/nix.nix
@@ -1,4 +1,13 @@
 { pkgs, ... }: {
+  # TODO remove in next nixpkgs update
+  nixpkgs.overlays = [
+    (_: pkgs: {
+      nix-output-monitor =
+        let url = "github:nixos/nixpkgs/18536bf04cd71abd345f9579158841376fdd0c5a"; in
+        (builtins.getFlake url).legacyPackages.${pkgs.system}.nix-output-monitor;
+    })
+  ];
+
   nix = {
     package = pkgs.lib.mkForce pkgs.lix;
 
@@ -10,4 +19,21 @@
       use-xdg-base-directories = true;
     };
   };
+
+  home-manager.sharedModules = [{
+    home.shellAliases = {
+      n = "nix repl"; # i use this often, so make it short!
+      nb = "nom build";
+      ncc = "nix flake check -L"; # nc is netcat
+      ne = "nix eval -L";
+      nej = "nix eval -L --raw --apply builtins.toJSON";
+      ng = "nix store gc -v";
+      ni = "nix flake init";
+      nm = "nix flake metadata";
+      nn = "nix flake new";
+      nr = "nix run -L";
+      ns = "nix flake show";
+      nu = "nix flake update";
+    };
+  }];
 }
