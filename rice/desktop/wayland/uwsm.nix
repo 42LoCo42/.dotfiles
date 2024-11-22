c51eb002dc40559b2a9fd5c7bbf572cf7a1958b2
Leon Schumacher <leonsch@protonmail.com>
2024-11-22 18:16:34 +0100

    rice/desktop/wayland: use USWM

---
+++ rice/desktop/wayland/uwsm.nix
@@ -0,0 +1,10 @@
+{
+  programs.uwsm = {
+    enable = true;
+    waylandCompositors.hyprland = {
+      prettyName = "Hyprland";
+      comment = "Hyprland compositor managed by UWSM";
+      binPath = "/run/current-system/sw/bin/Hyprland";
+    };
+  };
+}
