6baf53a5bcd9ce5e917e5a73b22607de75697869
Leon Schumacher <leonsch@protonmail.com>
2024-11-22 14:05:18 +0100

    rice: clean home of firefox directories

--- rice/desktop/firefox.nix
+++ rice/desktop/firefox.nix
@@ -1,17 +1,5 @@
-{ lib, config, ... }: lib.mkIf config.rice.desktop {
-  nixpkgs.overlays = [
-    (_: pkgs: {
-      firefox = pkgs.firefox.override { cfg.speechSynthesisSupport = false; };
-    })
-  ];
-
-  # TODO build declarative ffox config
+{
   home-manager.sharedModules = [{
-    programs.firefox.enable = true;
-
-    systemd.user.tmpfiles.rules = [
-      "d %h/.local/share/pki       0700 - - - -"
-      "d %h/.local/share/pki/nssdb 0700 - - - -"
-    ];
+    aquaris.firefox.enable = true;
   }];
 }
