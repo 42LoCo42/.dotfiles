8baff75ec6843f00aa4bab3c4c081c9eaf2f4e36
Leon Schumacher <leonsch@protonmail.com>
2024-12-10 11:05:37 +0100

    Update Aquaris

--- rice/common/nix.nix
+++ rice/common/nix.nix
@@ -1,34 +1,7 @@
-{ pkgs, ... }: {
+{
   nix = {
-    package = pkgs.lib.mkForce pkgs.lix;
-
+    # TODO does this actually do anything?
     daemonCPUSchedPolicy = "idle";
     daemonIOSchedClass = "idle";
-
-    settings = {
-      keep-going = true;
-      use-xdg-base-directories = true;
-    };
   };
-
-  environment.systemPackages = with pkgs; [ nix-tree ];
-
-  home-manager.sharedModules = [{
-    home.shellAliases = {
-      n = "nix repl"; # i use this often, so make it short!
-      nb = "nom build";
-      ncc = "nix flake check -L"; # nc is netcat
-      ne = "nix eval -L";
-      nej = "nix eval -L --raw --apply builtins.toJSON";
-      ner = "nix eval -L --raw";
-      ng = "nix store gc -v";
-      ni = "nix flake init";
-      nm = "nix flake metadata";
-      nn = "nix flake new";
-      nr = "nix run -L";
-      ns = "nix flake show";
-      nt = "nix-tree";
-      nu = "nix flake update";
-    };
-  }];
 }
