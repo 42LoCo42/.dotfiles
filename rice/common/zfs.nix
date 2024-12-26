faa2061eef5ccc22be629ab1913b8941629141af
Leon Schumacher <leonsch@protonmail.com>
2024-12-26 18:32:40 +0100

    rice/common/zfs: add zfsnaps command

--- rice/common/zfs.nix
+++ rice/common/zfs.nix
@@ -1,7 +1,20 @@
-{
+{ pkgs, ... }: {
   services.zfs = {
     autoScrub.enable = true;
     autoSnapshot.enable = true;
     trim.enable = true;
   };
+
+  environment.systemPackages = [
+    (pkgs.writeShellApplication {
+      name = "zfsnaps";
+      text = ''
+        zfs list -t snapshot -o name,used,refer \
+        | sed -E '
+          s|(snap)_([^-]+)-([^ ]+)|\1-\3-\2|;
+          s| +| |g;' \
+        | sort | column -t -s ' ' | less
+      '';
+    })
+  ];
 }
