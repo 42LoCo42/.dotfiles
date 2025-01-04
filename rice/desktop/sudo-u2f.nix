23533c73483783f73af39b4a3d417ddc1e1cc0b0
Leon Schumacher <leonsch@protonmail.com>
2025-01-04 16:53:36 +0100

    rice/desktop/sudo-u2f: default to sshformat

--- rice/desktop/sudo-u2f.nix
+++ rice/desktop/sudo-u2f.nix
@@ -1,13 +1,23 @@
 { lib, config, ... }: {
-  options.rice.desktop.sudo-u2f.enable = lib.mkOption {
-    type = lib.types.bool;
-    default = false;
+  options.rice.desktop.sudo-u2f = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+
+    sshformat = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
   };
 
   config = lib.mkIf config.rice.desktop.sudo-u2f.enable {
     security.pam = {
       services.sudo.u2fAuth = true;
-      u2f.settings.cue = true;
+      u2f.settings = {
+        cue = true;
+        inherit (config.rice.desktop.sudo-u2f) sshformat;
+      };
     };
   };
 }
