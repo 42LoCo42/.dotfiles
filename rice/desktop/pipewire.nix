d858a56f4e5e57d406de34a9f90d181890882f22
Leon Schumacher <leonsch@protonmail.com>
2025-01-05 18:53:38 +0100

    rice/desktop/pipewire: add music EQ option

--- rice/desktop/pipewire.nix
+++ rice/desktop/pipewire.nix
@@ -1,7 +1,14 @@
-{ lib, config, ... }: {
-  options.rice.desktop.pipewire.enable = lib.mkOption {
-    type = lib.types.bool;
-    default = false;
+{ pkgs, lib, config, ... }: {
+  options.rice.desktop.pipewire = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+
+    eq = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
   };
 
   config = lib.mkIf config.rice.desktop.pipewire.enable {
@@ -23,8 +30,27 @@
       };
     };
 
-    home-manager.sharedModules = [{
-      aquaris.persist = { ".local/state/wireplumber" = { }; };
-    }];
+    home-manager.sharedModules = [
+      {
+        aquaris.persist = { ".local/state/wireplumber" = { }; };
+      }
+
+      (lib.mkIf config.rice.desktop.pipewire.eq {
+        services.easyeffects = {
+          enable = true;
+          preset = "default";
+        };
+
+        xdg.configFile."easyeffects/output/default.json".source = lib.pipe null [
+          (_: pkgs.fetchFromGitHub {
+            owner = "Digitalone1";
+            repo = "EasyEffects-Presets";
+            rev = "32d0f416e7867ccffdab16c7fe396f2522d04b2e";
+            hash = "sha256-U9SSyHOOs8GsV6GBEqAqlBAuYONeh/4nkK8HurkEfWk=";
+          })
+          (x: "${x}/LoudnessEqualizer.json")
+        ];
+      })
+    ];
   };
 }
