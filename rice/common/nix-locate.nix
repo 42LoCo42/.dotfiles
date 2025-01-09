555461581b2df377676c94c5d6e0b3164735cf04
Leon Schumacher <leonsch@protonmail.com>
2025-01-09 10:40:00 +0100

    rice/common/nix-locate: add aliases

--- rice/common/nix-locate.nix
+++ rice/common/nix-locate.nix
@@ -1,4 +1,11 @@
 { self, ... }: {
   imports = [ self.inputs.nix-index-database.nixosModules.nix-index ];
   programs.command-not-found.enable = false;
+
+  home-manager.sharedModules = [{
+    home.shellAliases = {
+      nl = "nix-locate --top-level";
+      nlr = "nl --regex";
+    };
+  }];
 }
