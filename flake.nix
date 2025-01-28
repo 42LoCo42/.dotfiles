760e7c55cdf67d2b1019012a64963580569203cb
Leon Schumacher <leonsch@protonmail.com>
2025-01-28 09:09:55 +0100

    Update Aquaris

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    aquaris.url = "github:42loco42/aquaris/secrets-no-ifd";
+    aquaris.url = "github:42loco42/aquaris";
     aquaris.inputs.home-manager.follows = "home-manager";
     aquaris.inputs.nixpkgs.follows = "nixpkgs";
     aquaris.inputs.obscura.follows = "obscura";
