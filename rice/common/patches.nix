b070c9a716ed991cab98f09591eaf00fadf14c4c
Leon Schumacher <leonsch@protonmail.com>
2025-02-14 09:35:16 +0100

    Update to pull in SearXNG patch

--- rice/common/patches.nix
+++ rice/common/patches.nix
@@ -8,44 +8,6 @@
           hash = "sha256-VxbaxqyIAwmjjbgfTajqwPQC3bp7g/JNVNx9yy/3tus=";
         };
       });
-
-      # TODO https://github.com/matt1432/nixos-configs/commit/4927481b255210aeda060e06d0a1a8cd68cd5ae1
-      searxng = pkgs.searxng.override {
-        # FIXME: https://github.com/NixOS/nixpkgs/issues/380351
-        python3 = pkgs.python3.override {
-          packageOverrides = _: pyPrev: {
-            httpx = pyPrev.httpx.overridePythonAttrs (o: rec {
-              version = "0.27.2";
-              src = pkgs.fetchFromGitHub {
-                owner = "encode";
-                repo = o.pname;
-                tag = version;
-                hash = "sha256-N0ztVA/KMui9kKIovmOfNTwwrdvSimmNkSvvC+3gpck=";
-              };
-            });
-
-            starlette = pyPrev.starlette.overridePythonAttrs (o: rec {
-              version = "0.41.2";
-              src = pkgs.fetchFromGitHub {
-                owner = "encode";
-                repo = "starlette";
-                tag = version;
-                hash = "sha256-ZNB4OxzJHlsOie3URbUnZywJbqOZIvzxS/aq7YImdQ0=";
-              };
-            });
-
-            httpx-socks = pyPrev.httpx-socks.overridePythonAttrs (o: rec {
-              version = "0.9.2";
-              src = pkgs.fetchFromGitHub {
-                owner = "romis2012";
-                repo = "httpx-socks";
-                tag = "v${version}";
-                hash = "sha256-PUiciSuDCO4r49st6ye5xPLCyvYMKfZY+yHAkp5j3ZI=";
-              };
-            });
-          };
-        };
-      };
     })
   ];
 }
