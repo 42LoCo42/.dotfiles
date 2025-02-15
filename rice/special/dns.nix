9ed7b0c1447a4fcda7f56d0cd60240703342a55c
Leon Schumacher <leonsch@protonmail.com>
2025-02-15 14:49:28 +0100

    rice/special/dns: listen on IPv6 too; only use anonymized DNSCrypt

--- rice/special/dns.nix
+++ rice/special/dns.nix
@@ -14,38 +14,90 @@
 
       dnscrypt-proxy2 = {
         enable = true;
-        upstreamDefaults = false;
+        upstreamDefaults = true;
         settings = {
-          listen_addresses = [ "127.0.0.1:53" ];
+          listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
 
-          query_log = {
-            file = "/dev/stdout";
-            format = "ltsv";
-          };
+          query_log.file = "/dev/stdout";
 
           ipv4_servers = true;
           ipv6_servers = true;
 
+          http3 = true;
+
           dnscrypt_servers = true;
-          doh_servers = true;
-          odoh_servers = true;
+          doh_servers = false;
+          odoh_servers = false;
 
           require_dnssec = true;
           require_nolog = true;
           require_nofilter = true;
 
-          http3 = true;
-
-          lb_strategy = "p2";
-          lb_estimator = true;
+          cache = true;
+          cache_size = 100000;
 
           bootstrap_resolvers = [ "9.9.9.9:53" ];
 
-          block_unqualified = true;
-          block_undelegated = true;
+          sources = {
+            public-resolvers = {
+              cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
+              minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+              refresh_delay = 73;
+              urls = [
+                "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+                "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+              ];
+            };
 
-          cache = true;
-          cache_size = 10000;
+            quad9-resolvers = {
+              cache_file = "/var/cache/dnscrypt-proxy/quad9-resolvers.md";
+              minisign_key = "RWQBphd2+f6eiAqBsvDZEBXBGHQBJfeG6G+wJPPKxCZMoEQYpmoysKUN";
+              prefix = "quad9-";
+              urls = [ "https://www.quad9.net/quad9-resolvers.md" ];
+            };
+
+            dnscry-pt-resolvers = {
+              cache_file = "/var/cache/dnscrypt-proxy/dnscry.pt-resolvers.md";
+              minisign_key = "RWQM31Nwkqh01x88SvrBL8djp1NH56Rb4mKLHz16K7qsXgEomnDv6ziQ";
+              prefix = "dnscry.pt-";
+              refresh_delay = 73;
+              urls = [ "https://www.dnscry.pt/resolvers.md" ];
+            };
+
+            ####################
+
+            relays = {
+              cache_file = "/var/cache/dnscrypt-proxy/relays.md";
+              minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+              refresh_delay = 73;
+              urls = [
+                "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md"
+                "https://download.dnscrypt.info/resolvers-list/v3/relays.md"
+              ];
+            };
+          };
+
+          # these are used for AnonDNS relaying
+          disabled_server_names = [
+            "cs-berlin"
+            "cs-de"
+            "cs-dus3"
+            "digitalprivacy.diy-dnscrypt-ipv4"
+          ];
+
+          anonymized_dns = {
+            routes = [{
+              server_name = "*";
+              via = [
+                "anon-cs-berlin"
+                "anon-cs-de"
+                "anon-cs-dus3"
+                "anon-digitalprivacy.diy-ipv4"
+              ];
+            }];
+
+            skip_incompatible = true;
+          };
 
           forwarding_rules = pkgs.writeText "forwarding-rules.txt" ''
             bunny.vpn    100.100.100.100
@@ -83,87 +135,6 @@
             172.31.*
             192.168.*
           '';
-
-          sources = {
-            public-resolvers = {
-              cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
-              minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-              refresh_delay = 73;
-              urls = [
-                "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-                "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-              ];
-            };
-
-            relays = {
-              cache_file = "/var/cache/dnscrypt-proxy/relays.md";
-              minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-              refresh_delay = 73;
-              urls = [
-                "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md"
-                "https://download.dnscrypt.info/resolvers-list/v3/relays.md"
-              ];
-            };
-
-            odoh-servers = {
-              cache_file = "/var/cache/dnscrypt-proxy/odoh-servers.md";
-              minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-              refresh_delay = 73;
-              urls = [
-                "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/odoh-servers.md"
-                "https://download.dnscrypt.info/resolvers-list/v3/odoh-servers.md"
-              ];
-            };
-
-            odoh-relays = {
-              cache_file = "/var/cache/dnscrypt-proxy/odoh-relays.md";
-              minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-              refresh_delay = 73;
-              urls = [
-                "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/odoh-relays.md"
-                "https://download.dnscrypt.info/resolvers-list/v3/odoh-relays.md"
-              ];
-            };
-
-            quad9-resolvers = {
-              cache_file = "/var/cache/dnscrypt-proxy/quad9-resolvers.md";
-              minisign_key = "RWQBphd2+f6eiAqBsvDZEBXBGHQBJfeG6G+wJPPKxCZMoEQYpmoysKUN";
-              prefix = "quad9-";
-              urls = [ "https://www.quad9.net/quad9-resolvers.md" ];
-            };
-
-            dnscry-pt-resolvers = {
-              cache_file = "/var/cache/dnscrypt-proxy/dnscry.pt-resolvers.md";
-              minisign_key = "RWQM31Nwkqh01x88SvrBL8djp1NH56Rb4mKLHz16K7qsXgEomnDv6ziQ";
-              prefix = "dnscry.pt-";
-              refresh_delay = 73;
-              urls = [ "https://www.dnscry.pt/resolvers.md" ];
-            };
-          };
-
-          broken_implementations.fragments_blocked = [
-            "cisco"
-            "cisco-ipv6"
-            "cisco-familyshield"
-            "cisco-familyshield-ipv6"
-            "cisco-sandbox"
-            "cleanbrowsing-adult"
-            "cleanbrowsing-adult-ipv6"
-            "cleanbrowsing-family"
-            "cleanbrowsing-family-ipv6"
-            "cleanbrowsing-security"
-            "cleanbrowsing-security-ipv6"
-          ];
-
-          # TODO is this a good idea?
-          anonymized_dns = {
-            routes = [{
-              server_name = "*";
-              via = [ "*" ];
-            }];
-
-            skip_incompatible = true;
-          };
         };
       };
     };
