2cd4280df7f937c9eb1f56ee9c27ae87e4f1c1de
Leon Schumacher <leonsch@protonmail.com>
2025-03-05 22:23:32 +0100

    rice/desktop/wayland/hyprland: adapt safekill to librewolf

--- rice/desktop/wayland/hyprland/scripts/safekill.sh
+++ rice/desktop/wayland/hyprland/scripts/safekill.sh
@@ -3,7 +3,7 @@ json="$(hyprctl activewindow -j)"
 class="$(jq -r .class <<<"$json")"
 initialTitle="$(jq -r .initialTitle <<<"$json")"
 
-if [ "$class" = "firefox" ] && [ "$initialTitle" = "Mozilla Firefox" ]; then
+if [ "$class" = "librewolf" ] && [ "$initialTitle" = "LibreWolf" ]; then
 	hyprctl dispatch sendshortcut "ctrl, q,"
 else
 	hyprctl dispatch killactive
