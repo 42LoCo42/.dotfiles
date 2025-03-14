1ae3b0ae72e7dc1cb130f4d56e9c6f1733811ee7
Leon Schumacher <leonsch@protonmail.com>
2025-03-14 09:34:39 +0100

    rice/common/nix: add obscura to flake registry

--- rice/common/nix.nix
+++ rice/common/nix.nix
@@ -1,7 +1,14 @@
-{
+{ self, ... }: {
   nix = {
     # TODO does this actually do anything?
     daemonCPUSchedPolicy = "idle";
     daemonIOSchedClass = "idle";
+
+    registry.obscura.to = {
+      type = "github";
+      owner = "42LoCo42";
+      repo = "obscura";
+      inherit (self.inputs.obscura) rev;
+    };
   };
 }
