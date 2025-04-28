9949b0e5904d98743a25063ad3f65945a2f4b3aa
Leon Schumacher <leonsch@protonmail.com>
2025-04-28 21:55:24 +0200

    rice/special/pam-rssh: check if sockets are actually connected

--- rice/special/pam-rssh/default.nix
+++ rice/special/pam-rssh/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }: {
+{ pkgs, lib, config, aquaris, ... }: {
   options.rice.pam-rssh.enable = lib.mkOption {
     type = lib.types.bool;
     default = false;
@@ -15,7 +15,9 @@
     };
 
     home-manager.sharedModules = [{
-      home.file.".ssh/rc".source = ./ssh-rc.sh;
+      home.file.".ssh/rc".text = aquaris.lib.subsT ./ssh-rc.sh {
+        nc = lib.getExe pkgs.netcat;
+      };
 
       programs.zsh.envExtra = ''
         if [ -v TMUX ]; then export SSH_AUTH_SOCK="$HOME/.ssh/auth"; fi
--- rice/special/pam-rssh/ssh-rc.sh
+++ rice/special/pam-rssh/ssh-rc.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 link="$HOME/.ssh/auth"
-if [ ! -S "$link" ] && [ -S "$SSH_AUTH_SOCK" ]; then
+
+connected() {
+	test -S "$1" && @nc@ -zU "$1"
+}
+
+if (! connected "$link") && connected "$SSH_AUTH_SOCK"; then
 	ln -sf "$SSH_AUTH_SOCK" "$link"
 fi
