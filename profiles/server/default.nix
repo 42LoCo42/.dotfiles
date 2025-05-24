25cebb6db30049d436e2c0ce977e33649b803c38
Leon Schumacher <leonsch@protonmail.com>
2025-05-24 13:55:42 +0200

    tscaddy: update hash

--- profiles/server/default.nix
+++ profiles/server/default.nix
@@ -8,7 +8,7 @@
           "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556"
         ];
 
-        hash = "sha256-USKNTAvxmuxzhqA8e8XERr1U8513ONG54Md5vcDUERg=";
+        hash = "sha256-wt3+xCsT83RpPySbL7dKVwgqjKw06qzrP2Em+SxEPto=";
       };
     })
   ];
