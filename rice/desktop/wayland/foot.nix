f306a22dcc858b0a5880d82cce067d3bc84b33b8
Leon Schumacher <leonsch@protonmail.com>
2025-05-24 15:22:06 +0200

    getFlake be gone!

--- rice/desktop/wayland/foot.nix
+++ rice/desktop/wayland/foot.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }: {
+{ pkgs, lib, config, ... }: {
   options.rice.desktop.wayland.foot = {
     enable = lib.mkOption {
       type = lib.types.bool;
@@ -16,27 +16,13 @@
       programs.foot = {
         enable = true;
         settings = {
-          main.font = "monospace:size=10.5";
+          main = {
+            font = "monospace:size=10.5";
+            include = "${pkgs.foot.themes}/share/foot/themes/gruvbox-dark";
+          };
+
           colors = {
             inherit (config.rice.desktop.wayland.foot) alpha;
-            foreground = "ebdbb2";
-            background = "282828";
-            regular0 = "282828";
-            regular1 = "cc241d";
-            regular2 = "98971a";
-            regular3 = "d79921";
-            regular4 = "458588";
-            regular5 = "b16286";
-            regular6 = "689d6a";
-            regular7 = "a89984";
-            bright0 = "928374";
-            bright1 = "fb4934";
-            bright2 = "b8bb26";
-            bright3 = "fabd2f";
-            bright4 = "83a598";
-            bright5 = "d3869b";
-            bright6 = "8ec07c";
-            bright7 = "ebdbb2";
           };
         };
       };
