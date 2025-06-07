4f0294d9d2ca32c6a86bca07c15836d085422741
Leon Schumacher <leonsch@protonmail.com>
2025-06-07 09:36:28 +0200

    bunny/searxng: use anubis instead of builtin bot detection

--- machines/bunny/services/searxng/default.nix
+++ machines/bunny/services/searxng/default.nix
@@ -8,25 +8,44 @@
   '';
 
   virtualisation.pnoc.searxng = {
-    cmd = config.rice.redis ++ [
-      (lib.getExe pkgs.python3.pkgs.gunicorn)
-      "--bind=0.0.0.0:8080"
-      "--threads=4"
-      "--workers=4"
-      "searx.webapp:app"
+    cmd = [
+      (lib.getExe' pkgs.runit "runsvdir")
+      (config.rice.mkRunit {
+        anubis = ''
+          exec anubis                      \
+            --bind :8080                   \
+            --og-passthrough               \
+            --serve-robots-txt             \
+            --target http://localhost:8081 \
+            --redirect-domains searx.${config.rice.domain}
+        '';
+
+        searxng = ''
+          exec gunicorn         \
+            --bind 0.0.0.0:8081 \
+            --threads 4         \
+            --workers 4         \
+            searx.webapp:app
+        '';
+      }).outPath
     ];
 
     environment = {
-      PYTHONPATH = pkgs.searxng.pythonModule.pkgs.makePythonPath [ pkgs.searxng ];
+      PATH = lib.makeBinPath (with pkgs; with python3.pkgs; [
+        anubis
+        gunicorn
+        runit
+      ]);
+
+      PYTHONPATH = pkgs.searxng.pythonModule.pkgs.makePythonPath
+        (with pkgs; [ searxng ]);
+
       SEARXNG_BASE_URL = "https://searx.${config.rice.domain}";
     };
 
     environmentFiles = [ (config.aquaris.secret "@machine/searxng") ];
 
     volumes = [
-      "searxng:/data"
-
-      "${./limiter.toml}:/etc/searxng/limiter.toml:ro"
       "${./settings.yaml}:/etc/searxng/settings.yml:ro"
     ];
   };
