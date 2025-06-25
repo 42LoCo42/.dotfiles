07a2dce27f8ae5843a87ab9117d614f751a3762a
Leon Schumacher <leonsch@protonmail.com>
2025-06-25 08:27:37 +0200

    rice/common/patches: waybar-ipfix is no longer needed

--- rice/common/patches.nix
+++ rice/common/patches.nix
@@ -16,11 +16,6 @@
         pam_rssh = obscura.pam_rssh_next; # v1.2.0 shows prompt on authentication
         swaybg = obscura.swaybg_webp; # support for webp images
 
-        # default waybar always shows IPv6 in network module
-        # https://github.com/Alexays/Waybar/pull/3959
-        # https://github.com/Alexays/Waybar/commit/5e4dac1c0aebd6c4ad1f358f09e1cfd06a95d529
-        waybar = obscura.waybar-ipfix;
-
         inherit (obscura)
           caddyfile-language-server
           chronometer
