dd32b3786e0b04e89717b2f86f5e451b50af07aa
Leon Schumacher <leonsch@protonmail.com>
2025-07-25 14:20:04 +0200

    rice/desktop/emacs: add greeter/splashscreen

--- rice/desktop/emacs/default.nix
+++ rice/desktop/emacs/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, aquaris, ... }:
 let
   inherit (lib) mkForce mkIf mkOption remove;
   inherit (lib.types) bool str;
@@ -52,71 +52,11 @@ in
 
         usePackage.statistics = true;
 
-        prelude = ''
-          (defvar my/temp-dir (concat user-emacs-directory "temp"))
-
-          (defun my/join-line ()
-            (interactive)
-            (join-line)
-            (forward-line 1)
-            (back-to-indentation))
-
-          (defun my/smart-home ()
-            "Jump to beginning of line or first non-whitespace."
-            (interactive)
-            (let ((oldpos (point)))
-              (back-to-indentation)
-              (and (= oldpos (point)) (beginning-of-line))))
-
-          (defun my/autosplit ()
-            (interactive)
-            (if (> 0 (- (* 8 (window-total-width)) (* 20 (window-total-height))))
-              (my/split-switch-below)
-              (my/split-switch-right)))
-
-          (defun my/split-switch-below ()
-            "Split and switch to window below."
-            (interactive)
-            (split-window-below)
-            (balance-windows)
-            (other-window 1))
-
-          (defun my/split-switch-right ()
-            "Split and switch to window on the right."
-            (interactive)
-            (split-window-right)
-            (balance-windows)
-            (other-window 1))
-
-            (defun my/haskell-reload ()
-              (interactive)
-              (haskell-process-file-loadish
-               "reload" t
-               (or haskell-interactive-previous-buffer (current-buffer))))
-
-          (defun my/notify (msg)
-            "Send a graphical notification."
-            (start-process "notify" nil "notify-send" "emacs" msg))
-
-          (defun my/startup-notify ()
-            "Notify about startup time."
-            (my/notify (format "Startup took %s!" (emacs-init-time))))
-
-          (defun my/consult-imenu-or-outline ()
-            "Run consult-imenu or consult-outline depending on major-mode"
-            (interactive)
-            (pcase major-mode
-              ('org-mode (consult-outline))
-              (_ (consult-imenu))))
-
-          (defun my/align-regexp ()
-            "Run align-regexp with indent-tabs disabled."
-            (interactive)
-            (let ((state indent-tabs-mode))
-              (indent-tabs-mode -1)
-              (call-interactively #'align-regexp)
-              (indent-tabs-mode (if state 1 -1))))
-        '';
+        prelude = aquaris.lib.subsT ./prelude.el {
+          splash = builtins.path {
+            path = ./splash.webp;
+          };
+        };
 
         postlude = ''
           (setq cua-remap-control-v nil)
---
+++ rice/desktop/emacs/prelude.el
@@ -0,0 +1,95 @@
+(defvar my/temp-dir (concat user-emacs-directory "temp"))
+
+(defun my/join-line ()
+  (interactive)
+  (join-line)
+  (forward-line 1)
+  (back-to-indentation))
+
+(defun my/smart-home ()
+  "Jump to beginning of line or first non-whitespace."
+  (interactive)
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point)) (beginning-of-line))))
+
+(defun my/autosplit ()
+  (interactive)
+  (if (> 0 (- (* 8 (window-total-width)) (* 20 (window-total-height))))
+    (my/split-switch-below)
+    (my/split-switch-right)))
+
+(defun my/split-switch-below ()
+  "Split and switch to window below."
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
+
+(defun my/split-switch-right ()
+  "Split and switch to window on the right."
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+
+(defun my/haskell-reload ()
+  (interactive)
+  (haskell-process-file-loadish
+   "reload" t
+   (or haskell-interactive-previous-buffer (current-buffer))))
+
+(defun my/notify (msg)
+  "Send a graphical notification."
+  (start-process "notify" nil "notify-send" "emacs" msg))
+
+(defun my/startup-notify ()
+  "Notify about startup time."
+  (my/notify (format "Startup took %s!" (emacs-init-time))))
+
+(defun my/consult-imenu-or-outline ()
+  "Run consult-imenu or consult-outline depending on major-mode"
+  (interactive)
+  (pcase major-mode
+    ('org-mode (consult-outline))
+    (_ (consult-imenu))))
+
+(defun my/align-regexp ()
+  "Run align-regexp with indent-tabs disabled."
+  (interactive)
+  (let ((state indent-tabs-mode))
+    (indent-tabs-mode -1)
+    (call-interactively #'align-regexp)
+    (indent-tabs-mode (if state 1 -1))))
+
+(defun my/greeter ()
+  "Switches to a simple buffer which greets the user."
+  (interactive)
+
+  (switch-to-buffer "*greeter*")
+  (special-mode)
+  (read-only-mode -1)
+  (erase-buffer)
+
+  (display-fill-column-indicator-mode -1) ; hide fill column
+  (display-line-numbers-mode -1)          ; hide line numbers
+  (call-interactively #'hl-line-mode)     ; can't be directly called for some reason
+  (centaur-tabs-local-mode 1)             ; unintuitively *dis*ables tab bar
+  (font-lock-mode -1)                     ; must be disabled for propertize to work
+  (setq cursor-type nil)                  ; hide cursor
+  (setq mode-line-format nil)             ; hide modeline
+
+  (let* ((img (create-image "@splash@"))
+         (greeting (propertize "Welcome to Emacs!" 'face '(:height 4.0 :foreground "white")))
+         (cw (frame-char-width))
+         (gw (string-pixel-width greeting))
+         (gs (+ (/ gw cw) (if (zerop (% gw cw)) 0 1))))
+
+    (insert-char ?\n 8)
+    (insert-image img (propertize "a" 'line-prefix `(space . (:align-to (- center (0.5 . ,img))))))
+
+    (insert-char ?\n 4)
+    (insert (propertize greeting 'line-prefix `(space . (:align-to (- center (0.5 . ,gs)))))))
+
+  (read-only-mode 1)
+  (message nil))
