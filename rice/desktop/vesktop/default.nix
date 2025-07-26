ab88879460a468c8c8d41b2b069f574e1ad8077f
Leon Schumacher <leonsch@protonmail.com>
2025-07-26 09:59:21 +0200

    Update

--- rice/desktop/vesktop/default.nix
+++ rice/desktop/vesktop/default.nix
@@ -14,16 +14,11 @@
         package = pkgs.vesktop.override {
           vencord = pkgs.vencord.overrideAttrs (old: {
             patches = (old.patches or [ ]) ++ [
+              ./add-moyai-plugin.patch
               ./add-smirk-plugin.patch
             ];
 
             preBuild = (old.preBuild or "") + ''
-              # restore moyai plugin
-              patch -p1 --reverse --input ${pkgs.fetchurl {
-                url = "https://github.com/Vendicated/Vencord/commit/600a95f751c5977f47d64aaa97fdbfd3f324504e.patch";
-                hash = "sha256-8GcZub1Ot1rSXqMxRBgsL2zIy0hr2I1J1iOxHaNIVpA=";
-              }}
-
               # allow media-src for raw.githubusercontent.com
               # so that moyai and smirk can load sounds
               sed -i '${lib.replaceString "\n" "" ''
