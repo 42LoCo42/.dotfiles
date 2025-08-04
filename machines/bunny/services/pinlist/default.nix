1c05fa4f5913d9c9c972b2794736ec4232689b43
Leon Schumacher <leonsch@protonmail.com>
2025-08-04 11:40:38 +0200

    machines/bunny: pinlist: OIDC support

--- machines/bunny/services/pinlist/default.nix
+++ machines/bunny/services/pinlist/default.nix
@@ -1,6 +1,13 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, config, ... }: {
   virtualisation.pnoc.pinlist = {
-    cmd = [ (lib.getExe pkgs.pinlist) ];
-    volumes = [ "pinlist:/db" ];
+    cmd = [ (lib.getExe pkgs.pinlist) "/data/pinlist.db" ];
+
+    environment = {
+      PINLIST_OIDC_ISSUER = "https://id.eleonora.gay";
+    };
+
+    environmentFiles = [ (config.aquaris.secret "@machine/pinlist") ];
+
+    volumes = [ "pinlist:/data" ];
   };
 }
