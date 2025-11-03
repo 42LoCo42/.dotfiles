02243ead02aff30e53ef801014c5bb3107038d8e
Leon Schumacher <leonsch@protonmail.com>
2025-11-03 23:20:03 +0100

    Update

--- rice/desktop/vesktop/default.nix
+++ rice/desktop/vesktop/default.nix
@@ -11,37 +11,26 @@
         enable = true;
         vencord.useSystem = true;
 
-        package =
-          let
-            vencord = pkgs.vencord.overrideAttrs (old: {
-              patches = (old.patches or [ ]) ++ [
-                ./add-moyai-plugin.patch
-                ./add-smirk-plugin.patch
-              ];
-
-              preBuild = (old.preBuild or "") + ''
-                # allow media-src for raw.githubusercontent.com
-                # so that moyai and smirk can load sounds
-                sed -i '${lib.replaceString "\n" "" ''
-                  /"raw.githubusercontent.com"/
-                  s|ImageAndCssSrc|[...ImageAndCssSrc, "media-src"]|
-                ''}' src/main/csp/index.ts
-              '';
-            });
-          in
-          (pkgs.vesktop.override {
-            inherit vencord;
-            withSystemVencord = true;
-            withTTS = false;
-          }).overrideAttrs {
-            # TODO https://pr-tracker.bunny/?pr=457209
-            patches = [
-              (pkgs.path + /pkgs/by-name/ve/vesktop/fix_read_only_settings.patch)
-              (pkgs.replaceVars ./use_system_vencord.patch {
-                inherit vencord;
-              })
+        package = pkgs.vesktop.override {
+          vencord = pkgs.vencord.overrideAttrs (old: {
+            patches = (old.patches or [ ]) ++ [
+              ./add-moyai-plugin.patch
+              ./add-smirk-plugin.patch
             ];
-          };
+
+            preBuild = (old.preBuild or "") + ''
+              # allow media-src for raw.githubusercontent.com
+              # so that moyai and smirk can load sounds
+              sed -i '${lib.replaceString "\n" "" ''
+                /"raw.githubusercontent.com"/
+                s|ImageAndCssSrc|[...ImageAndCssSrc, "media-src"]|
+              ''}' src/main/csp/index.ts
+            '';
+          });
+
+          withSystemVencord = true;
+          withTTS = false;
+        };
       };
     }];
   };
