1cca050c3cc1a1b8fa303159c72f66c55e2bd968
Leon Schumacher <leonsch@protonmail.com>
2025-12-03 10:43:49 +0100

    Update

--- profiles/server/default.nix
+++ profiles/server/default.nix
@@ -8,7 +8,7 @@
           "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556"
         ];
 
-        hash = "sha256-cZWBui1q+DuzuerrM2CD6RAdtmp56cIwCPyfS1LchzI=";
+        hash = "sha256-cJlffHv9d1xna4/feb/Q4VilTnlxnKBTtIyd8oGdQOQ=";
       }).overrideAttrs { doCheck = false; };
     })
   ];
