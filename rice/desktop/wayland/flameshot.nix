3fffa504c0c05fb5a7a1dd86535a1d1247b258b4
Leon Schumacher <leonsch@protonmail.com>
2026-04-26 10:16:08 +0200

    rice/d/w/flameshot: wait instead of busylooping

--- rice/desktop/wayland/flameshot.nix
+++ rice/desktop/wayland/flameshot.nix
@@ -11,16 +11,19 @@
         slurp
         wl-clipboard
 
+        # pin flameshot to v13.3.0 because v14 removes the grim adapter
+        # and the stupid dbus screenshot protocol is borked :(
+        ((import (fetchTarball {
+          url = "https://github.com/nixos/nixpkgs/tarball/4c1018dae018162ec878d42fec712642d214fdfa";
+          sha256 = "sha256-ar3rofg+awPB8QXDaFJhJ2jJhu+KqN/PRCXeyuXR76E=";
+        })) { inherit (pkgs.stdenv) system; }).flameshot
+
         (pkgs.writeShellApplication {
           name = "flameshot-run";
           text = ''
-            ${lib.getExe pkgs.flameshot} gui -r | wl-copy &
-
-            while true; do
-              [ "$(hyprctl activewindow -j | jq -r .initialTitle)" = flameshot ] && break
-            done
+            flameshot gui -r | wl-copy &
+            grep -m1 flameshot < <(nc -U "$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock")
             pkill -SIGUSR1 waybar
-
             wait
             pkill -SIGUSR1 waybar
           '';
