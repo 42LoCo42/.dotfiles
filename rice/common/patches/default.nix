bb85a504919372c9f92c8cd92f22fb92d9b85a98
Leon Schumacher <leonsch@protonmail.com>
2026-06-09 08:29:54 +0200

    rice/common/patches: pull my-nom from obscura

--- rice/common/patches/default.nix
+++ rice/common/patches/default.nix
@@ -4,27 +4,17 @@
     self.inputs.obscura.lib.infuse prev ({
       ########## temporary overrides ##########
 
-      nix-output-monitor.__output = {
-        version.__assign = "2.1.8-unstable-2026-05-22";
-
-        src.__assign = prev.fetchFromGitHub {
-          owner = "maralorn";
-          repo = "nix-output-monitor";
-          rev = "0e855e51c1700e35456faa3dee2e50024f602f42";
-          hash = "sha256-8viiPvLkj0vFdG1kgcNuKXoenyTBvKd+GQ62jwbONns=";
-        };
-      };
-
       # TODO upstream hyprlandPlugins aren't compatible with 0.54.* yet
       # https://hydra.nixos.org/job/nixpkgs/unstable/hyprlandPlugins.hyprfocus.x86_64-linux
       # https://hydra.nixos.org/job/nixpkgs/unstable/hyprlandPlugins.hyprwinwrap.x86_64-linux
       hyprland.__assign = self.inputs.obscura.inputs.nixpkgs.legacyPackages.${prev.stdenv.system}.hyprland;
       hyprlandPlugins.__assign = obscura.my-hypr-plugins.entries;
 
-      ergochat  .__assign = obscura.my-ergochat; ## TODO https://pr-tracker.bunny/?pr=502133
-      gamja     .__assign = obscura.my-gamja; ##### TODO https://codeberg.org/emersion/gamja/pulls/210
-      hydroxide .__assign = obscura.my-hydroxide; # TODO https://github.com/emersion/hydroxide/pull/138
-      prettypst .__assign = obscura.my-prettypst; # TODO https://github.com/antonWetzel/prettypst/issues/11
+      ergochat           .__assign = obscura.my-ergochat; ## TODO https://pr-tracker.bunny/?pr=502133
+      gamja              .__assign = obscura.my-gamja; ##### TODO https://codeberg.org/emersion/gamja/pulls/210
+      hydroxide          .__assign = obscura.my-hydroxide; # TODO https://github.com/emersion/hydroxide/pull/138
+      nix-output-monitor .__assign = obscura.my-nom; ####### TODO nom needs new release
+      prettypst          .__assign = obscura.my-prettypst; # TODO https://github.com/antonWetzel/prettypst/issues/11
 
       ########## permanent overrides ##########
 
