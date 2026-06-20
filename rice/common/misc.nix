ee67cf38457f56133d812f094a577df8773a76bf
Leon Schumacher <leonsch@protonmail.com>
2026-06-20 11:30:14 +0200

    rice/common/misc: tie systemd-machined to libvirtd

--- rice/common/misc.nix
+++ rice/common/misc.nix
@@ -1,10 +1,10 @@
-{ lib, ... }: {
+{ config, lib, ... }: {
   services.nscd.enable = false;
   system.nssModules = lib.mkForce [ ];
 
   services.speechd.enable = false;
 
-  systemd.services.systemd-machined.enable = false;
+  systemd.services.systemd-machined.enable = config.virtualisation.libvirtd.enable;
 
   home-manager.sharedModules = [{
     xdg.configFile."go/telemetry/mode".text = "off 1970-01-01";
