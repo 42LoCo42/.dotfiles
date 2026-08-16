b89e2940e72747ea205f983bdc1799244179b4b7
Eleonora <leonsch@protonmail.com>
2026-08-16 20:28:22 +0200

    machines/bunny/pocket-id: fix email delivery

--- machines/bunny/services/pocket-id/default.nix
+++ machines/bunny/services/pocket-id/default.nix
@@ -12,7 +12,17 @@
   '';
 
   virtualisation.pnoc.pocket-id = {
-    cmd = [ (lib.getExe pkgs.pocket-id) ];
+    path = with pkgs; [ socat pocket-id ];
+
+    # HACK HACK HACK
+    # pocket-id sends one-time code emails with github:italypaleale/go-kit/emailer
+    # which refuses to send credentials over unencrypted, non-local connections
+    # this is digusting security theater and deserves naught but shame
+    # bypass it by making socat forward everything to msmtpd
+    script = ''
+      socat TCP-LISTEN:2525,fork,reuseaddr TCP-CONNECT:msmtpd:2525 &
+      exec pocket-id
+    '';
 
     environment = {
       ANALYTICS_DISABLED = "true";
@@ -27,7 +37,7 @@
       EMAIL_ONE_TIME_ACCESS_AS_ADMIN_ENABLED = "true";
       EMAIL_API_KEY_EXPIRATION_ENABLED = "true";
 
-      SMTP_HOST = "msmtpd";
+      SMTP_HOST = "localhost"; # see note above
       SMTP_PORT = "2525";
       SMTP_FROM = "pocket-id@${config.rice.domain}";
       SMTP_USER = "user";
