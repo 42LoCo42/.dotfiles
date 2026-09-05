212fedf657895ec738f5fe0f2cd75c6aa7b79873
Eleonora <leonsch@protonmail.com>
2026-09-05 13:39:06 +0200

    rice/desktop/gomuks: switch from pnoc to systemd service

--- rice/desktop/gomuks.nix
+++ rice/desktop/gomuks.nix
@@ -20,21 +20,27 @@ in
   config = mkIf cfg.enable {
     rice.insecureNames = [ "olm" ];
 
-    virtualisation.pnoc = {
-      gomuks-web = {
-        path = with pkgs; [ gomuks-web ];
+    aquaris.persist.dirs = {
+      "/var/lib/private/gomuks-web" = { };
+    };
 
-        script = ''
-          exec gomuks-web << EOF
-          admin
-          admin
-          EOF
-        '';
+    systemd.services.gomuks-web = {
+      path = with pkgs; [ gomuks-web ];
+      script = ''
+        export GOMUKS_ROOT="$STATE_DIRECTORY"
+        exec gomuks-web << EOF
+        admin
+        admin
+        EOF
+      '';
 
-        environment.GOMUKS_ROOT = "/data";
-        ports = [ "29325:29325" ];
-        volumes = [ "gomuks-web:/data" ];
+      serviceConfig = {
+        Type = "simple";
+        DynamicUser = true;
+        StateDirectory = "gomuks-web";
       };
+
+      wantedBy = [ "default.target" ];
     };
 
     home-manager.sharedModules = singleton (hm: {
